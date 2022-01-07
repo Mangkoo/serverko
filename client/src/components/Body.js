950f3938689eba8d5ee6042c327fc60c5fbae0d6
Robert <oconnor.robert.james@gmail.com>
2022-01-07 19:03:05 +1100

    Cache/ Package Lock

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -6,29 +6,75 @@ function Body() {
   return (
     <div className='body'>
           
-      <div className='bodyCardContainer'>
-        <div className='bodyCardHeader'>Community Posts</div>    
-          <div className='bodyCard'>
+      <div className='communityCardContainer'>
+        <div className='communityCardHeader'>Community</div>   
+
+          <div className='communityCard'>
             
-            <ul>  
-              
-              <li className='bodyListItem'>        
-                <h1 class='bodyHeader'> <i class="fas fa-wrench"></i> This page is still under construction..</h1>
-              </li>            
+            <ul className='bodyList'>  
+         
               
               <li className='bodyListItem'>
-                <body class='bodyText'>Check back here soon!</body>
+                <body class='bodyText'>Check out what the Social Servers Community has to say!</body>
+              </li>
+
+              <li className='bodyListItem'>
+                <div className='demoFeed'>
+                  <i>Whoops! Looks like we're having trouble contacting the Social Servers API 😟 </i>
+                </div>
+              </li>
+
+            </ul>
+
+          </div>
+
+        </div>
+
+        <div className='recentCardContainer'>
+        <div className='recentCardHeader'>Recent Posts</div>    
+          <div className='recentCard'>
+            
+            <ul>  
+                        
+              
+              <li className='bodyListItem'>
+                <body class='bodyText'>Most Recent:</body>
+              </li>
+
+              <li className='bodyListItem'>
+              <div className='demoFeed'>
+                  <ul>
+                    <li className='bodyListItem'>
+                     <body class='bodyText'><i class="fas fa-comment-alt"></i> demo_post_1</body>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+
+             
+
+              <li className='bodyListItem'>
+                <body class='bodyText'><i class="fas fa-comment-alt"></i> demo_post_2</body>
+              </li>
+
+              <li className='bodyListItem'>
+                <body class='bodyText'><i class="fas fa-comment-alt"></i> demo_post_3</body>
+              </li>
+              
+              <li className='bodyListItem'>
+                <body class='bodyText'><i class="fas fa-comment-alt"></i> demo_post_4</body>
               </li>
 
 
 
+
+
             </ul>
-              
-              
+
           </div>
-          </div>
-     
-    </div>
+        </div>
+      </div>
   );
 }
 
